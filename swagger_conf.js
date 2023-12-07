44d84a1861be47486e02b160ef106e278a4d42e0
PaulofmRodrigues <paulofilipe_100pt@hotmail.com>
2023-12-07 17:37:23 +0000

    Update swagger_conf.js

--- swagger_conf.js
+++ swagger_conf.js
@@ -1,7 +1,7 @@
 const options = {
     swaggerDefinition: {
         info: {
-            description: 'Mocc API for ESMAPP project',
+            description: 'Mooc API for CAMUL project',
             title: "Mooc API",
             version: "1.0.0"
         },
